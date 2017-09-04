1abe77824221e1e9885c45a7d21787a55424ff8d
Jonas Reich <mail@jonasreich.de>
2017-09-05 00:48:46 +0200

    Auto refresh git overlay

---
+++ Assets/Plugins/git-integration/Editor/Git.cs
@@ -0,0 +1,243 @@
+//-------------------------------------------
+// (c) 2017 - Jonas Reich
+//-------------------------------------------
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEditor;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+namespace GitIntegration
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	[InitializeOnLoad]
+	public static class Git
+	{
+		public static string output = "";
+		public static Process process;
+		public static List<File> files = new List<File>();
+
+		static Texture texture;
+		static bool dirty = false;
+		static string currentSelectionPath = "";
+
+
+		static Git()
+		{
+			EditorApplication.projectWindowItemOnGUI += ProjectWindowItemOnGui;
+			EditorApplication.projectWindowChanged += delegate
+			{
+				dirty = true;
+			};
+			texture = Resources.Load<Texture>("GitIcons/added");
+
+			RefreshStatus();
+		}
+
+
+		public static void Update()
+		{
+			if (dirty && IsReady())
+			{
+				dirty = false;
+				RefreshStatus();
+				if (GitEditorWindow.Window)
+					GitEditorWindow.Window.Repaint();
+			}
+
+			if (UpdateCurrentSelectionPath())
+			{
+				dirty = true;
+			}
+
+			// -
+
+			if (process != null && process.HasExited == false)
+			{
+				if (process.StartInfo.Arguments.Contains("status --porcelain"))
+				{
+					string line = "";
+
+					while (process.StandardOutput.EndOfStream == false)
+					{
+						line = process.StandardOutput.ReadLine();
+						File file = new File();
+
+						file.status_string = line.Substring(0, 2);
+						file.path += line.Substring(3);
+
+						file.path = file.path.Replace("\"", "");
+						file.path = file.path.Trim();
+
+						{
+							var substrings = file.path.Split('/');
+							if (file.path.EndsWith("/"))
+								file.name = substrings[substrings.Length - 2] + "/";
+							else
+								file.name = substrings[substrings.Length - 1];
+						}
+
+						file.path = "\"" + file.path + "\"";
+
+						if (file.status_string.StartsWith("#"))
+						{
+							continue;
+						}
+
+						if (file.status_string[0] == '?')
+						{
+							file.status |= (uint)EStatus.Untracked;
+						}
+						else if (file.status_string[0] != ' ')
+						{
+							file.status |= (uint)EStatus.HasStagedChanges;
+						}
+
+						if (file.status_string[0] == 'R')
+						{
+							file.status |= (uint)EStatus.Renamed;
+						}
+						else if (file.status_string[0] == 'D')
+						{
+							file.status |= (uint)EStatus.Deleted;
+						}
+
+						if (line[1] != ' ')
+						{
+							file.status |= (uint)EStatus.HasUnstagedChanges;
+						}
+
+						files.Add(file);
+					}
+				}
+				else
+				{
+
+					string newLine = process.StandardOutput.ReadToEnd();
+					if (newLine != "")
+					{
+						Debug.Log(newLine);
+						output += newLine;
+					}
+					while (process.StandardError.EndOfStream == false)
+					{
+						string newError = process.StandardError.ReadLine();
+						if (newError != "")
+						{
+							if (newError.Contains("warning:") && newError.Contains("will be replaced by"))
+							{
+								newError += "\n" + process.StandardError.ReadLine();
+								Debug.LogWarning(newError);
+							}
+							else
+							{
+								Debug.LogError(newError);
+							}
+							output += newError;
+						}
+					}
+
+				}
+			}
+		}
+
+		public static bool IsReady()
+		{
+			return (process != null && process.HasExited == true) || process == null;
+		}
+
+		public static void Command(string arguments, bool setDirty = true)
+		{
+			process = new Process();
+			process.StartInfo.FileName = "git";
+			process.StartInfo.Arguments = arguments;
+			process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+			process.StartInfo.CreateNoWindow = true;
+			process.StartInfo.RedirectStandardError = true;
+
+			process.StartInfo.RedirectStandardOutput = true;
+			process.StartInfo.UseShellExecute = false;
+
+			process.Start();
+
+			if (setDirty)
+				dirty = true;
+		}
+
+		public static void RefreshStatus()
+		{
+			Command("status --porcelain", false);
+			files.Clear();
+		}
+
+
+		static bool UpdateCurrentSelectionPath()
+		{
+			var path = "";
+			var obj = Selection.activeObject;
+			if (obj == null) path = "Assets";
+			else path = AssetDatabase.GetAssetPath(obj.GetInstanceID());
+			if (currentSelectionPath == path)
+			{
+				return false;
+			}
+			else
+			{
+				currentSelectionPath = path;
+				return true;
+			}
+		}
+
+		static void ProjectWindowItemOnGui(string guid, Rect selectionRect)
+		{
+			var path = AssetDatabase.GUIDToAssetPath(guid);
+			//if (!AssetDatabase.IsValidFolder(path)) return;
+
+			selectionRect.height = selectionRect.width = 16;
+
+			foreach (var file in files)
+			{
+				if (path.Contains(file.path.Replace("\"", "")))
+					GUI.DrawTexture(selectionRect, texture);
+			}
+		}
+
+
+		public class File
+		{
+			public string name;
+			public string path;
+			public string status_string;
+			public uint status = (uint)EStatus.None;
+
+			public bool HasStatus(EStatus status)
+			{
+				return (this.status & (uint)status) == (uint)status;
+			}
+		}
+
+		[Flags]
+		public enum EStatus : uint
+		{
+			None = 0,
+			Untracked = 1 << 0,
+
+			Unmodified = 1 << 1,
+			Modified = 1 << 2,
+
+			HasStagedChanges = 1 << 3,
+			HasUnstagedChanges = 1 << 4,
+
+			Deleted = 1 << 5,
+			Renamed = 1 << 6,
+			Copied = 1 << 7,
+
+			Unmerged = 1 << 8
+		}
+	}
+}
--- Assets/Plugins/git-integration/Editor/GitEditorWindow.cs
+++ Assets/Plugins/git-integration/Editor/GitEditorWindow.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Runtime.Remoting.Messaging;
 using UnityEditor;
@@ -20,138 +21,48 @@ namespace GitIntegration
 	/// </summary>
 	public class GitEditorWindow : EditorWindow
 	{
-		static GitEditorWindow Window;
-		static Texture texture;
-
+		public static GitEditorWindow Window;
+		
 		[MenuItem("Window/Git")]
 		static void OpenWindow()
 		{
 			Window = GetWindow<GitEditorWindow>("Git");
-			EditorApplication.projectWindowItemOnGUI -= ProjectWindowItemOnGui;
-			EditorApplication.projectWindowItemOnGUI += ProjectWindowItemOnGui;
-			texture = Resources.Load<Texture>("GitIcons/added");
-		}
-
-		static void ProjectWindowItemOnGui(string guid, Rect selectionRect)
-		{
-			var path = AssetDatabase.GUIDToAssetPath(guid);
-			//if (!AssetDatabase.IsValidFolder(path)) return;
-
-			selectionRect.height = selectionRect.width = 16;
-
-			foreach (var windowGitFile in Window.gitFiles)
-			{
-				if(path.Contains(windowGitFile.path.Replace("\"","")))
-					GUI.DrawTexture(selectionRect, texture);
-			}
 		}
 		
-
-		string gitOutput = "";
+		bool repaintAsap = false;
 		string commitMessage = "";
-		Process gitProcess;
 		Vector2 outputScrollPosition, stageScrollPosition, unstageScrollPosition;
-		List<GitFile> gitFiles = new List<GitFile>();
-
+		
 		void OnGUI()
 		{
-			if (GUILayout.Button("status") && IsGitReady())
+			if (GUILayout.Button("test"))
 			{
-				RefreshStatus();
-			}
-
-			if (GUILayout.Button("status log") && IsGitReady())
-			{
-				GitCommand("status");
-			}
-
-			if (gitProcess != null && gitProcess.HasExited == false)
-			{
-				if (gitProcess.StartInfo.Arguments.Contains("status -s"))
-				{
-					string line = "";
-
-					while (gitProcess.StandardOutput.EndOfStream == false)
-					{
-						line = gitProcess.StandardOutput.ReadLine();
-						GitFile file = new GitFile();
-						
-						file.status_string = line.Substring(0, 2);
-						file.path += line.Substring(3);
-
-						file.path = file.path.Replace("\"", "");
-						file.path = file.path.Trim();
-
-						var substrings = file.path.Split('/');
-						if (file.path.EndsWith("/"))
-							file.name = substrings[substrings.Length - 2] + "/";
-						else
-							file.name = substrings[substrings.Length - 1];
-
-						file.path = "\"" + file.path + "\"";
-
-						if (file.status_string.StartsWith("#"))
-						{
-							continue;
-						}
-
-						if (file.status_string.Contains("?"))
-						{
-							file.status |= (uint)EGitStatus.Untracked;
-						}
-						if (file.status_string.Contains("A"))
-						{
-							file.status |= (uint)EGitStatus.HasStagedChanges;
-						}
-						if (line[1] != ' ')
-						{
-							file.status |= (uint) EGitStatus.HasUnstagedChanges;
-						}
-
-						gitFiles.Add(file);
-					}
-				}
-				else
-				{
-
-					string newLine = gitProcess.StandardOutput.ReadToEnd();
-					if (newLine != "")
-					{
-						Debug.Log(newLine);
-						gitOutput += newLine;
-					}
-					string newError = gitProcess.StandardError.ReadToEnd();
-					if (newError != "")
-					{
-						gitOutput += newError;
-						Debug.LogError(newError);
-					}
-				}
+				Git.Command("status --porcelain");
 			}
 
 			EditorGUILayout.Space();
 
 			EditorGUILayout.BeginHorizontal();
 			GUILayout.Label("Staged Changes");
-			if (GUILayout.Button(new GUIContent("-", "Unstage all files"), GUILayout.Width(20)) && IsGitReady())
+			if (GUILayout.Button(new GUIContent("-", "Unstage all files"), GUILayout.Width(20)) && Git.IsReady())
 			{
-				GitCommand("reset .");
-				repaint_asap = true;
+				Git.Command("reset .");
+				repaintAsap = true;
 			}
 			EditorGUILayout.EndHorizontal();
 			stageScrollPosition = GUILayout.BeginScrollView(stageScrollPosition, "TextArea", GUILayout.Height(150));
-			foreach (var gitFile in gitFiles)
+			foreach (var gitFile in Git.files)
 			{
-				if (gitFile.HasStatus(EGitStatus.HasStagedChanges))
+				if (gitFile.HasStatus(Git.EStatus.HasStagedChanges))
 				{
 					EditorGUILayout.BeginHorizontal();
 					EditorGUILayout.LabelField(gitFile.status_string, GUILayout.Width(30));
 					GUILayout.Label(gitFile.name, GUILayout.Width(300));
 					if (GUILayout.Button("-", GUILayout.Width(20)))
 					{
-						GitCommand("reset " + gitFile.path);
+						Git.Command("reset " + gitFile.path);
 						EditorGUILayout.EndHorizontal();
-						repaint_asap = true;
+						repaintAsap = true;
 						break;
 					}
 					EditorGUILayout.EndHorizontal();
@@ -163,25 +74,25 @@ namespace GitIntegration
 
 			EditorGUILayout.BeginHorizontal();
 			GUILayout.Label("Changes");
-			if (GUILayout.Button(new GUIContent("+", "Stage all changed files"), GUILayout.Width(20)) && IsGitReady())
+			if (GUILayout.Button(new GUIContent("+", "Stage all changed files"), GUILayout.Width(20)) && Git.IsReady())
 			{
-				GitCommand("add .");
-				repaint_asap = true;
+				Git.Command("add .");
+				repaintAsap = true;
 			}
 			EditorGUILayout.EndHorizontal();
 			unstageScrollPosition = GUILayout.BeginScrollView(unstageScrollPosition, "TextArea", GUILayout.Height(150));
-			foreach (var gitFile in gitFiles)
+			foreach (var gitFile in Git.files)
 			{
-				if (gitFile.HasStatus(EGitStatus.HasUnstagedChanges))
+				if (gitFile.HasStatus(Git.EStatus.HasUnstagedChanges))
 				{
 					EditorGUILayout.BeginHorizontal();
 					EditorGUILayout.LabelField(gitFile.status_string, GUILayout.Width(30));
 					GUILayout.Label(gitFile.name, GUILayout.Width(300));
 					if (GUILayout.Button("+", GUILayout.Width(20)))
 					{
-						GitCommand("add " + gitFile.path);
+						Git.Command("add " + gitFile.path);
 						EditorGUILayout.EndHorizontal();
-						repaint_asap = true;
+						repaintAsap = true;
 						break;
 					}
 					EditorGUILayout.EndHorizontal();
@@ -190,16 +101,16 @@ namespace GitIntegration
 			GUILayout.EndScrollView();
 
 			EditorGUILayout.Space();
-			
-			commitMessage = EditorGUILayout.TextArea(commitMessage);
+
+			commitMessage = EditorGUILayout.TextArea(commitMessage, GUILayout.Height(75));
 			EditorGUILayout.BeginHorizontal();
 			if (GUILayout.Button("Commit"))
 			{
 				if (commitMessage != "")
 				{
-					GitCommand("commit -m \"" + commitMessage + "\"");
+					Git.Command("commit -m \"" + commitMessage + "\"");
 					commitMessage = "";
-					repaint_asap = true;
+					repaintAsap = true;
 				}
 				else
 				{
@@ -214,87 +125,29 @@ namespace GitIntegration
 			GUILayout.Label("Log:");
 			if (GUILayout.Button("Clear", GUILayout.Width(80)))
 			{
-				gitOutput = "";
+				Git.output = "";
 			}
-			if (gitOutput.Length > 15000)
+			if (Git.output.Length > 15000)
 			{
-				gitOutput = gitOutput.Substring(gitOutput.Length - 15000);
+				Git.output = Git.output.Substring(Git.output.Length - 15000);
 			}
 
 			EditorGUILayout.EndHorizontal();
 			outputScrollPosition = GUILayout.BeginScrollView(outputScrollPosition, GUILayout.Height(150));
-			GUILayout.TextArea(gitOutput, GUILayout.ExpandHeight(true));
+			GUILayout.TextArea(Git.output, GUILayout.ExpandHeight(true));
 			GUILayout.EndScrollView();
 		}
-
-		private void RefreshStatus()
-		{
-			GitCommand("status -s");
-			gitFiles.Clear();
-			Repaint();
-		}
-
-		bool repaint_asap = false;
-
-		void GitCommand(string arguments)
-		{
-			gitProcess = new Process();
-			gitProcess.StartInfo.FileName = "git";
-			gitProcess.StartInfo.Arguments = arguments;
-			gitProcess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
-			gitProcess.StartInfo.RedirectStandardError = true;
-
-			gitProcess.StartInfo.RedirectStandardOutput = true;
-			gitProcess.StartInfo.UseShellExecute = false;
-
-			gitProcess.Start();
-		}
-
-		bool IsGitReady()
-		{
-			return (gitProcess != null && gitProcess.HasExited == true) || gitProcess == null;
-		}
-
+		
 		void Update()
 		{
-			if (repaint_asap && IsGitReady())
+			if (repaintAsap && Git.IsReady())
 			{
-				RefreshStatus();
-				repaint_asap = false;
+				Git.RefreshStatus();
+				repaintAsap = false;
 				Repaint();
 			}
+
+			Git.Update();
 		}
-	}
-
-	class GitFile
-	{
-		public string name;
-		public string path;
-		public string status_string;
-		public uint status = (uint)EGitStatus.None;
-
-		public bool HasStatus(EGitStatus status)
-		{
-			return (this.status & (uint) status) == (uint)status;
-		}
-	}
-
-	[Flags]
-	enum EGitStatus : uint
-	{
-		None = 0,
-		Untracked = 1 << 0,
-
-		Unmodified = 1 << 1,
-		Modified = 1 << 2,
-
-		HasStagedChanges = 1 << 3,
-		HasUnstagedChanges = 1 << 4,
-
-		Deleted = 1 << 5,
-		Renamed = 1 << 6,
-		Copied = 1 << 7,
-		
-		Unmerged = 1 << 8
 	}
 }
