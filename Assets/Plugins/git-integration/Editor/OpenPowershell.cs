73b221c414a1e74fa1cd7146bd6f1d66fc0ee936
Jonas Reich <mail@jonasreich.de>
2017-09-09 15:31:00 +0200

    Add context menu item to open a selected project folder in Powershell

---
+++ Assets/Plugins/git-integration/Editor/OpenPowershell.cs
@@ -0,0 +1,33 @@
+﻿//-------------------------------------------
+// (c) 2017 - Jonas Reich
+//-------------------------------------------
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEditor;
+using UnityEngine;
+
+namespace GitIntegration
+{
+	/// <summary>
+	/// Adds context menu item to open a selected project folder in Powershell
+	/// </summary>
+	public static class OpenPowershell
+	{
+		[MenuItem("Assets/Open in Powershell")]
+		static bool IsSelectedFolderValid()
+		{
+			return AssetDatabase.IsValidFolder(AssetDatabase.GetAssetPath(Selection.activeObject.GetInstanceID()));
+		}
+
+		[MenuItem("Assets/Open in Powershell")]
+		static void OpenSelectedFolderInPowershell()
+		{
+			Process p = new Process();
+			p.StartInfo.FileName = "powershell";
+			p.StartInfo.WorkingDirectory = AssetDatabase.GetAssetPath(Selection.activeObject.GetInstanceID());
+			p.Start();
+		}
+	}
+}
